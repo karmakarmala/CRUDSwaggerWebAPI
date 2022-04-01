3c068b7eb40afd0aa43bf9f10ccb1a8761ca03ec
Mala Karmakar <mala.karmakar@accessworld.com>
2022-04-01 13:59:20 +0800

    Add project files.

---
+++ WebAPIApp/Controllers/DeviceStatusController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using WebAPIApp.Model;
+using WebAPIApp.Service;
+
+namespace WebAPIApp.Controllers
+{
+   
+    [Route("[controller]")]
+    [ApiController]
+    public class DeviceStatusController : Controller, IDeviceStatus
+    {
+
+        private readonly ILogger<DeviceStatusController> _logger;
+        private List<DeviceSensorDetails> _deviceSensorDetails;
+
+        public DeviceStatusController(ILogger<DeviceStatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/DeviceStatus/GetStatus/DeviceName
+        [HttpGet("GetStatus/{DeviceName}")]
+        public IActionResult GetStatus(string DeviceName)
+        {
+            var DeviceStatus = GetAllDeviceStatus().Where(x => x.DeviceName == DeviceName).FirstOrDefault();
+
+             if(DeviceStatus==null)
+            {
+                string message = string.Format("Mo Device found with Device Name: {0}", DeviceName);
+                return NotFound(message);
+            }
+            else
+            {
+                return Ok(DeviceStatus);
+            }
+          
+        }
+
+        [HttpGet("GetAllDeviceStatus")]
+        public IEnumerable<DeviceSensorDetails> GetAllDeviceStatus()
+        {
+            _deviceSensorDetails = new List<DeviceSensorDetails>()
+            {
+                new DeviceSensorDetails()
+                {
+                    DeviceName="HP Color LaserJet MFP M183fw",
+                    ModelNo="NPIBDA46B",
+                    DeviceStatus="Sleep mode is on.",
+                    MotorTemperature=30.5,
+                    BlackCartridgePercentage=20.0,
+                    CyanCartridgePercentage=40.0,
+                    MagentaCartridgePercentage=50.0,
+                    YellowCartridgePercentage=50.0
+
+                }
+            };
+
+            return _deviceSensorDetails;
+        }
+
+        [HttpPost("AddDeviceStatus")]
+        public IActionResult AddDeviceStatus(DeviceSensorDetails sensorDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            _deviceSensorDetails = new List<DeviceSensorDetails>();
+            _deviceSensorDetails.Add(sensorDetails);
+            return Ok("Added New Status");
+        }
+
+
+ 
+    }
+}
+
---
+++ WebAPIApp/Service/IDeviceStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIApp.Model;
+
+namespace WebAPIApp.Service
+{
+    interface IDeviceStatus
+    {
+        IEnumerable<DeviceSensorDetails> GetAllDeviceStatus();
+    }
+}
